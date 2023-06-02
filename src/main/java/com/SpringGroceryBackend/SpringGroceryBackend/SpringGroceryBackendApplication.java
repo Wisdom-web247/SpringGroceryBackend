76e2537bef8a3b7687a613e69cdb1e1a83f359b4
Wisdom Mahlangu <wizzymahlangu@gmail.com>
2023-06-02 10:58:08 +0200

    Initialised the Project without any Files and folders

---
+++ src/main/java/com/SpringGroceryBackend/SpringGroceryBackend/SpringGroceryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.SpringGroceryBackend.SpringGroceryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGroceryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGroceryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringGroceryBackend/SpringGroceryBackend/SpringGroceryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringGroceryBackend.SpringGroceryBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGroceryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
